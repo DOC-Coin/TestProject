b1a381c3d51a826366555860b0fb9cc187482e8f
Rinzler <33075641+Docfully@users.noreply.github.com>
2025-07-11 16:22:09 -0700

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,33 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Jacques Stroud on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
